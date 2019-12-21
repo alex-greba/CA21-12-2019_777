dca7da1694db9dc80639c927491ce9cdeafab7a9
Alex <54438028+alex-greba@users.noreply.github.com>
2019-12-21 16:47:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA21_12_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!!!");
+        }
+    }
+}
