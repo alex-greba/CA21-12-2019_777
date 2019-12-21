85ce9c030d6f8bdf1bc954496ac57596cb2bea90
Alex <54438028+alex-greba@users.noreply.github.com>
2019-12-21 17:54:36 +0300

    Add files via upload

---
+++ InjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA21_12_2019
+{
+    class CtionConfig
+    {
+    }
+}
